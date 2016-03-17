901dfd4801b242e45336d19f339b6f41d92af218
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2016-03-16 20:28:40 -0400

    full department, latex, DataSpace link, class year is pub year

--- lib/orangetheses/fetcher.rb
+++ lib/orangetheses/fetcher.rb
@@ -60,6 +60,8 @@ module Orangetheses
         h = {}
         h['id'] = i['handle'][/[^\/]*$/]
         i['metadata'].each do |m|
+          m['value'] = map_department(m['value']) if m['key'] == 'pu.department'
+          next if m['value'].nil?
           if h[m['key']].nil?
             h[m['key']] = [m['value']]
           else
@@ -85,6 +87,63 @@ module Orangetheses
       resp = Faraday.get "#{@server}/communities/#{community_id}/collections"
       json = JSON.parse(resp.body)
       json.map {|i| i['id'].to_s}
+    end
+
+    def map_department(dept)
+      lc_authorized_departments[dept]
+    end
+
+    def lc_authorized_departments
+      {
+        "Art and Archaeology" => "Princeton University. Department of Art and Archaeology",
+        "Aeronautical Engineering" => "Princeton University. Department of Aeronautical Engineering",
+        "Anthropology" => "Princeton University. Department of Anthropology",
+        "Architecture School" => "Princeton University. School of Architecture",
+        "Astrophysical Sciences" => "Princeton University. Department of Astrophysical Sciences",
+        "Biochemical Sciences" => "Princeton University. Department of Biochemical Sciences",
+        "Biology" => "Princeton University. Department of Biology",
+        "Civil and Environmental Engineering" => "Princeton University. Department of Civil and Environmental Engineering",
+        "Civil Engineering and Operations Research" => "Princeton University. Department of Civil Engineering and Operations Research",
+        "Chemical and Biological Engineering" => "Princeton University. Department of Chemical and Biological Engineering",
+        "Chemistry" => "Princeton University. Department of Chemistry",
+        "Classics" => "Princeton University. Department of Classics",
+        "Comparative Literature" => "Princeton University. Department of Comparative Literature",
+        "Computer Science" => "Princeton University. Department of Computer Science",
+        "Creative Writing Program" => "Princeton University. Creative Writing Program ",
+        "East Asian Studies" => "Princeton University. Department of East Asian Studies",
+        "Economics" => "Princeton University. Department of Economics",
+        "Ecology and Evolutionary Biology" => "Princeton University. Department of Ecology and Evolutionary Biology",
+        "Electrical Engineering" => "Princeton University. Department of Electrical Engineering",
+        "Engineering and Applied Science" => "Princeton University. School of Engineering and Applied Science",
+        "English" => "Princeton University. Department of English",
+        "French and Italian" => "Princeton University. Department of French and Italian",
+        "Geosciences" => "Princeton University. Department of Geosciences",
+        "German" => "Princeton University. Department of Germanic Languages and Literatures",
+        "History" => "Princeton University. Department of History",
+        "Special Program in Humanities" => "Princeton University. Special Program in the Humanities",
+        "Independent Concentration" => "Princeton University Independent Concentration Program ",
+        "Mathematics" => "Princeton University. Department of Mathematics",
+        "Molecular Biology" => "Princeton University. Department of Molecular Biology",
+        "Mechanical and Aerospace Engineering" => "Princeton University. Department of Mechanical and Aerospace Engineering",
+        "Medieval Studies" => "Princeton University. Program in Medieval Studies",
+        "Modern Languages" => "Princeton University. Department of Modern Languages.",
+        "Music" => "Princeton University. Department of Music",
+        "Near Eastern Studies" => "Princeton University. Department of Near Eastern Studies",
+        "Operations Research and Financial Engineering" => "Princeton University. Department of Operations Research and Financial Engineering",
+        "Oriental Studies" => "Princeton University. Department of Oriental Studies",
+        "Philosophy" => "Princeton University. Department of Philosophy",
+        "Physics" => "Princeton University. Department of Physics",
+        "Politics" => "Princeton University. Department of Politics",
+        "Psychology" => "Princeton University. Department of Psychology",
+        "Religion" => "Princeton University. Department of Religion",
+        "Romance Languages and Literatures" => "Princeton University. Department of Romance Languages and Literatures",
+        "Slavic Languages and Literature" => "Princeton University. Department of Slavic Languages and Literatures",
+        "Sociology" => "Princeton University. Department of Sociology",
+        "Spanish and Portuguese Languages and Cultures" => "Princeton University. Department of Spanish and Portuguese Languages and Cultures",
+        "Statistics" => "Princeton University. Department of Statistics",
+        "Theater" => "Princeton University. Program in Theater",
+        "Woodrow Wilson School" => "Woodrow Wilson School of Public and International Affairs"
+      }
     end
 
   end
